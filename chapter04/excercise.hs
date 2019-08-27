7b625d94e3f438b65f202cb196fa5ca92cb81808
shinharad <shinharad.github@gmail.com>
2019-08-27 10:24:57 +0900

    chap 04, 05 excercise

--- chapter04/excercise.hs
+++ chapter04/excercise.hs
@@ -63,5 +63,35 @@ b || c | b == c = b
 -- ----------------------------------------------
 -- 5.
 
+func :: Bool -> Bool -> Bool
+func a b =
+  if a then
+    if b then True else False
+  else False
+
+-- ----------------------------------------------
+-- 6.
+
+func' :: Bool -> Bool -> Bool
+func' a b = if a then b else False
+
+-- ----------------------------------------------
+-- 7.
+
+mult :: Int -> Int -> Int -> Int
+mult = \x -> (\y -> (\z -> (x * y * z)))
+
+-- ----------------------------------------------
+-- 8.
+
+luhnDouble :: Int -> Int
+luhnDouble x | n < 10    = n
+             | otherwise = n - 9
+  where n = x*2
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn w x y z = ((luhnDouble w) + x + (luhnDouble y) + z) `mod` 10 == 0
+
+
 
 
--- chapter05/excercise.hs
+++ chapter05/excercise.hs
@@ -1,25 +1,53 @@
+import Data.Char
+
+-- ----------------------------------------------
 -- 1.
+
 main1 = do
   print $ sum [ x^2 | x <- [1..100]]
 
+-- ----------------------------------------------
 -- 2.
+
 grid :: Int -> Int -> [(Int, Int)]
 grid m n = [ (x, y) | x <- [0..m], y <- [0..n]]
 
+-- ----------------------------------------------
 -- 3.
+
 square :: Int -> [(Int, Int)]
 square n = [ (x, y) | (x, y) <- grid n n, x /= y]
 
+-- ----------------------------------------------
 -- 4.
+
 replicate' :: Int -> a -> [a]
 replicate' n x = [ x | _ <- [1..n]]
 
+-- ----------------------------------------------
 -- 5.
+
 pyths :: Int -> [(Int, Int, Int)]
 pyths n = [ (x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
 
+-- ----------------------------------------------
+-- 6.
 
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (init (factors x)) == x]
+
+-- ----------------------------------------------
+-- 7.
+
+reexpressed :: [(Int,Int)]
+reexpressed = concat [[(x,y) | y <- [3,4]] | x <- [1,2]]
+
+-- ----------------------------------------------
 -- 8.
+
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [i | (x',i) <- zip xs [0..], x == x']
 
@@ -28,4 +56,37 @@ positions' x xs = map snd xs'
   where
     xs' = filter (\(a, b) -> a == x) (zip xs [0..])
 
+-- ----------------------------------------------
+-- 9.
 
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [x*y | (x,y) <- zip xs ys]
+
+-- ----------------------------------------------
+-- 10.
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+let2int' :: Char -> Int
+let2int' c = ord c - ord 'A'
+
+int2let :: Int -> Char
+int2let n = chr (ord 'a' + n)
+
+int2let' :: Int -> Char
+int2let' n = chr (ord 'A' + n)
+
+shift :: Int -> Char -> Char
+shift n c
+    | isLower c = int2let ((let2int c + n) `mod` 26)
+    | isUpper c = int2let' ((let2int' c + n) `mod` 26)
+    | otherwise = c
+
+encode :: Int -> String -> String
+encode n xs = [shift n x | x <- xs]
+
+main10 = do
+  let encoded = encode 3 "HASKELL IS FUN"
+  print $ encoded
+  print $ encode (-3) encoded
